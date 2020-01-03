638cf1ef0fa84105020883a9b605ed775e010d17
yilian <linyilian@4399inc.com>
2020-01-03 18:37:20 +0800

    56566565

--- src/main/java/com/rong/im/utils/TimeUtils.java
+++ src/main/java/com/rong/im/utils/TimeUtils.java
@@ -11,7 +11,6 @@ public class TimeUtils {
             return 0;
         }
 
-        
         String timestamp = String.valueOf(date.getTime());
         int length = timestamp.length();
         if (length > 3) {
